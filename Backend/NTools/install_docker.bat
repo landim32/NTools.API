7d0c2b97053f3ab445a98174424c597a51e1583b
Rodrigo Landim <rodrigo.carneiro@benner.com.br>
2025-09-11 16:52:56 -0300

    Add ACL

--- Backend/NTools/install_docker.bat
+++
@@ -1,2 +0,0 @@
-docker build -t crosschainswap-api -f BTCSTXSwap.API\Dockerfile .
-docker run -p 8080:80 crosschainswap-api
--- install_docker.sh
+++ install_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker stop nauth-api1
-docker rm nauth-api1
-cd ./Backend/NAuth/NAuth.API
-docker build -t nauth-api -f Dockerfile .
-docker run --name nauth-api1 -e ASPNETCORE_URLS="https://+" -e ASPNETCORE_HTTPS_PORTS=443 --network docker-network nauth-api &
+docker stop ntools-api1
+docker rm ntools-api1
+cd ./Backend/NTools/NTools.API
+docker build -t ntools-api -f Dockerfile .
+docker run --name ntools-api1 -e ASPNETCORE_URLS="https://+" -e ASPNETCORE_HTTPS_PORTS=443 --network docker-network ntools-api &
 docker ps
