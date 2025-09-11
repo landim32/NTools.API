7ae9a1944e51b6ecb51760a6525300b2d2e0d9fe
Rodrigo Landim <rodrigo.carneiro@benner.com.br>
2025-09-11 17:14:24 -0300

    Add ACL

--- install_docker.sh
+++ install_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker stop ntools-api1
 docker rm ntools-api1
-cd ./Backend/NTools/NTools.API
-docker build -t ntools-api -f Dockerfile .
+cd ./Backend/NTools
+docker build -t ntools-api -f ./NTools.API/Dockerfile .
 docker run --name ntools-api1 -e ASPNETCORE_URLS="https://+" -e ASPNETCORE_HTTPS_PORTS=443 --network docker-network ntools-api &
 docker ps
